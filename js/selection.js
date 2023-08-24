c6fec323235dabc0f1ff3da34899c8c46e28b533
Skiziks <nicolas.lerond@gmail.com>
2023-08-24 11:32:52 +0200

    ajout du fichier test

--- js/selection.js
+++ js/selection.js
@@ -1,28 +1,28 @@
 var btnWeb = document.querySelector('#btnWeb');
 var btnJeux = document.querySelector('#btnJeux');
-var btnGraph = document.querySelector('#btnGraph');
+// var btnGraph = document.querySelector('#btnGraph');
 var btnAud = document.querySelector('#btnAud');
 
 var sectionWeb = document.querySelector('#Web');
 var sectionJeux = document.querySelector('#jeux');
-var sectionGraph = document.querySelector('#Graph');
+// var sectionGraph = document.querySelector('#Graph');
 var sectionAud = document.querySelector('#Aud');
 
 btnWeb.addEventListener('click', WebAppear);
 btnJeux.addEventListener('click', JeuxAppear);
-btnGraph.addEventListener('click', GraphAppear);
+// btnGraph.addEventListener('click', GraphAppear);
 btnAud.addEventListener('click', AudAppear);
 
 function WebAppear() {
     sectionWeb.classList.remove('disappear');
     sectionJeux.classList.add('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.add('disappear');
 }
 function JeuxAppear() {
     sectionWeb.classList.add('disappear');
     sectionJeux.classList.remove('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.add('disappear');
 }
 function GraphAppear() {
@@ -34,7 +34,7 @@ function GraphAppear() {
 function AudAppear() {
     sectionWeb.classList.add('disappear');
     sectionJeux.classList.add('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.remove('disappear');
 }
 
@@ -50,27 +50,27 @@ function AudAppear() {
 const params = new URLSearchParams(window.location.search);
 const sectionName = params.get('section');
 
-if (sectionName==='jeux') {
+if (sectionName === 'jeux') {
     sectionWeb.classList.add('disappear');
     sectionJeux.classList.remove('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.add('disappear');
 }
-else if (sectionName==='Web') {
+else if (sectionName === 'Web') {
     sectionWeb.classList.remove('disappear');
     sectionJeux.classList.add('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.add('disappear');
 }
-else if (sectionName==='Graph') {
+else if (sectionName === 'Graph') {
     sectionWeb.classList.add('disappear');
     sectionJeux.classList.add('disappear');
     sectionGraph.classList.remove('disappear');
     sectionAud.classList.add('disappear');
 }
-else if (sectionName==='Aud') {
+else if (sectionName === 'Aud') {
     sectionWeb.classList.add('disappear');
     sectionJeux.classList.add('disappear');
-    sectionGraph.classList.add('disappear');
+    // sectionGraph.classList.add('disappear');
     sectionAud.classList.remove('disappear');
 }
